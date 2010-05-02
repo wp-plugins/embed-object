45e5f040c5349e5c384eb32077108816a64f483f
wribeiro <wribeiro@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-05-02 16:27:20 +0000

    Para funcionar em qualquer conteúdo do blog, incluindo feeds rss.
    git-svn-id: https://plugins.svn.wordpress.org/embed-object/tags/1.3@236644 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ embedObject.php
@@ -0,0 +1,113 @@
+<?php
+/*
+Plugin Name: Embed Object
+Plugin URI: http://blog.idealmind.com.br/wordpress/embed-object-wordpress-plugin-youtube-flash/
+Description: Este plugin permite que você insira objetos flash em seu blog, inclusive passando parâmetros por flashvars.<br />This plugin let's you embed flash objects in your blog and use flashvars.
+Version: 1.1
+Author: Wellington Ribeiro
+Author URI: http://blog.idealmind.com.br
+
+1.0   - Initial release
+*/
+Class embedObject
+{
+	private static $debug;
+	
+	public static function justDoIt( $content, $debug = false )
+	{
+		self::$debug = $debug;
+		$pattern = "/\[embeded:([^\]]+)\]/";
+		preg_match_all( $pattern, $content, $objects );
+
+		if( self::$debug )
+			self::debugArray($objects[0]);
+
+		foreach( $objects[0] as $k=>$object )
+		{
+			$content = str_replace( $object, self::getObject( $objects[1][$k] ), $content );
+		}
+		return $content;
+	}
+	
+	private static function getObject( $attributes )
+	{
+		$attr = $attributes;
+		
+		preg_match_all('| ([^\=]+)\\="([^\"]+)"|u',$attributes,$out);
+		
+		if( self::$debug )
+			self::debugArray($out);
+		
+		$wh = "";
+		$src = "";
+		$params = "";
+		$parametros = "";
+		
+		if( self::$debug )
+			echo "<pre>";
+		
+		foreach( $out[1] as $k=>$param)
+		{
+			$param = trim( $param );
+			$value = trim( $out[2][$k] );
+			
+			if( self::$debug )
+				echo "<b>Par:</b> $param \t\t\t <b>Val</b> $value<br />";
+			
+			if( $param == "width" || $param == "height" )
+			{
+				$wh .= " $param=\"$value\" ";
+			}
+			elseif( $param == "src" || $param == "movie" )
+			{
+				$src = "$param=\"$value\"";
+				$parametros .= "<param name=\"movie\" value=\"$value\"></param>\n";
+			}
+			else
+			{
+				$params .= " $param=\"$value\"";
+				$parametros .= "<param name=\"$param\" value=\"$value\"></param>\n";
+			}
+		}
+		
+		if( self::$debug )
+			echo "</pre>";
+		
+		$object = "
+			<object " . trim( $wh ) . ">
+				$parametros
+				<embed $src $params ". trim( $wh ) . "></embed>
+			</object>
+		";
+		
+		if( self::$debug )
+			echo "<pre>" . htmlentities( $object ) . "</pre>";
+		
+		if( in_array( "visualizar", $out[1] ) )
+		{
+			$attr = preg_replace( '/visualizar="([^\"]+)"/i', "", $attr );
+			return "[embeded: $attr]";
+		}
+		
+		return $object;
+	}
+	
+	private static function debugArray( $array )
+	{
+		echo "<pre>".print_r( $array, true )."</pre>";
+	}
+}
+
+function embedObject( $content )
+{
+	if( isset( $_GET['debug'] ) )
+	{
+		$debug = true;
+	}
+	$content = embedObject::justDoIt( $content, $debug );
+	return $content;
+}
+
+add_filter ('the_content', 'embedObject');
+
+?>
